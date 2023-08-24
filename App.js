618e428178fc37eb1298478fbf9c0f1ca9ccaa59
Tejas <guptatejas65@gmail.com>
2023-08-24 15:17:41 +0530

    PVRAPP SCRATCH PART DONE.

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Movie from './src/screens/Movie';
+
+const App = () => {
+  return <Movie />;
+};
+
+export default App;
---
+++ src/screens/Movie.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Movie = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Movie</Text>
+    </View>
+  );
+};
+
+export default Movie;
+
+const styles = StyleSheet.create({});
